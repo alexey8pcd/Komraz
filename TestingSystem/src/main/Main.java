fedf056e7ad8d53fe902a4e257cf3ff403919659
Alexey Ovcharov <alexey8rus@mail.ru>
2015-04-02 20:41:04 +0600

    Начало проекта

--- TestingSystem/src/main/Main.java
+++ TestingSystem/src/main/Main.java
@@ -3,11 +3,7 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.err.println("ashgchabc");
-        System.err.println("vnjznvjc");
-        System.err.println("vnjznvjc");
-        System.err.println("vnjznvvgvgvgjc");
+
     }
 
 }
