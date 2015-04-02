b411287ae6a4bebf42d78766e946bef785c34f68
ScanNorOne <ScanNorOne@air-dmitry>
2015-04-02 17:33:01 +0600

    HelloHelloHellobvdbdbdfb

--- TestingSystem/src/main/Main.java
+++ TestingSystem/src/main/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello");
         System.err.println("ashgchabc");
         System.err.println("vnjznvjc");
+        System.err.println("vnjznvjc");
     }
 
 }
