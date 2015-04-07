859d1eb483f048212a87a90f74464ce8cb8c859f
Alexey Ovcharov <alexey8rus@mail.ru>
2015-04-07 21:42:58 +0600

    Перенос строк

--- TestingSystem/src/entities/VoprosLatex.java
+++ TestingSystem/src/entities/VoprosLatex.java
@@ -23,9 +23,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Table(name = "vopros_latex")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "VoprosLatex.findAll", query = "SELECT v FROM VoprosLatex v"),
-    @NamedQuery(name = "VoprosLatex.findByIdVoprosLatex", query = "SELECT v FROM VoprosLatex v WHERE v.idVoprosLatex = :idVoprosLatex"),
-    @NamedQuery(name = "VoprosLatex.findByLatexZapis", query = "SELECT v FROM VoprosLatex v WHERE v.latexZapis = :latexZapis")})
+    @NamedQuery(name = "VoprosLatex.findAll", 
+            query = "SELECT v FROM VoprosLatex v"),
+    @NamedQuery(name = "VoprosLatex.findByIdVoprosLatex", 
+            query = "SELECT v FROM VoprosLatex v "
+                    + "WHERE v.idVoprosLatex = :idVoprosLatex"),
+    @NamedQuery(name = "VoprosLatex.findByLatexZapis", 
+            query = "SELECT v FROM VoprosLatex v "
+                    + "WHERE v.latexZapis = :latexZapis")})
 public class VoprosLatex implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -36,9 +41,12 @@ public class VoprosLatex implements Serializable {
     @Column(name = "LATEX_ZAPIS")
     private String latexZapis;
     @JoinColumns({
-        @JoinColumn(name = "VOPROS_ID_VOPROS", referencedColumnName = "ID_VOPROS"),
-        @JoinColumn(name = "VOPROS_KATEGORIYA_SLOZHNOSTI_ID_ KATEGORIYA_SLOZHNOSTI", referencedColumnName = "KATEGORIYA_SLOZHNOSTI_ID_ KATEGORIYA_SLOZHNOSTI"),
-        @JoinColumn(name = "VOPROS_TIP_VOPROSA_ID_ TIP_VOPROSA", referencedColumnName = "TIP_VOPROSA_ID_ TIP_VOPROSA")})
+        @JoinColumn(name = "VOPROS_ID_VOPROS", 
+                referencedColumnName = "ID_VOPROS"),
+        @JoinColumn(name = "VOPROS_KATEGORIYA_SLOZHNOSTI_ID_ KATEGORIYA_SLOZHNOSTI", 
+                referencedColumnName = "KATEGORIYA_SLOZHNOSTI_ID_ KATEGORIYA_SLOZHNOSTI"),
+        @JoinColumn(name = "VOPROS_TIP_VOPROSA_ID_ TIP_VOPROSA", 
+                referencedColumnName = "TIP_VOPROSA_ID_ TIP_VOPROSA")})
     @ManyToOne(optional = false)
     private Vopros vopros;
 
@@ -82,15 +90,13 @@ public class VoprosLatex implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof VoprosLatex)) {
             return false;
         }
         VoprosLatex other = (VoprosLatex) object;
-        if ((this.idVoprosLatex == null && other.idVoprosLatex != null) || (this.idVoprosLatex != null && !this.idVoprosLatex.equals(other.idVoprosLatex))) {
-            return false;
-        }
-        return true;
+        return !((this.idVoprosLatex == null && other.idVoprosLatex != null) || 
+                (this.idVoprosLatex != null &&
+                !this.idVoprosLatex.equals(other.idVoprosLatex)));
     }
 
     @Override
